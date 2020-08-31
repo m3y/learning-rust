c897661446f592052eb4e1c8653863a07fcb0f41
m3y <ma3ya.ozw+github@gmail.com>
2020-09-01 01:32:08 +0900

    using Clap with derive

--- samplecli/src/main.rs
+++ samplecli/src/main.rs
@@ -1,30 +1,28 @@
-use clap::{App, Arg};
+use clap::Clap;
+
+#[derive(Clap, Debug)]
+#[clap(
+    name = "My RPN program",
+    version = "1.0.0",
+    author = "Your name",
+    about = "Super awesome sample RPN calculator"
+)]
+struct Opts {
+    /// Sets the level of verbosity
+    #[clap(short, long)]
+    verbose: bool,
+
+    /// Formulas written in RPN
+    #[clap(name = "FILE")]
+    formula_file: Option<String>,
+}
 
 fn main() {
-    let matches = App::new("My RPN program")
-        .version("1.0.0")
-        .author("m3y")
-        .about("Super awesome sample RPN calculator")
-        .arg(
-            Arg::with_name("formula_file")
-                .about("Formulas written in RPN")
-                .value_name("FILE")
-                .index(1)
-                .required(false),
-        )
-        .arg(
-            Arg::with_name("verbose")
-                .about("Sets the level of verbosity")
-                .short('v')
-                .long("verbose")
-                .required(false),
-        )
-        .get_matches();
+    let opts = Opts::parse();
 
-    match matches.value_of("formula_file") {
+    match opts.formula_file {
         Some(file) => println!("File specified: {}", file),
         None => println!("No file specified."),
     }
-    let verbose = matches.is_present("verose");
-    println!("Is verbosity specified?: {}", verbose)
+    println!("Is verbosity specified?: {}", opts.verbose)
 }
