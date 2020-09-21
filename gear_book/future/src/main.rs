c430cefb5c7eb7a24373c3dbe422bb35fc375801
m3y <ma3ya.ozw+github@gmail.com>
2020-09-21 23:57:18 +0900

    small fix

--- gear_book/future/src/main.rs
+++ gear_book/future/src/main.rs
@@ -8,7 +8,7 @@ struct CountDown(u32);
 impl Future for CountDown {
     type Output = String;
 
-    fn poll(mut self: Pin<&mut Self>, cx: &mut Context) -> Poll<String> {
+    fn poll(mut self: Pin<&mut Self>, cx: &mut Context) -> Poll<Self::Output> {
         if self.0 == 0 {
             Poll::Ready("Zero!!!".to_string())
         } else {
