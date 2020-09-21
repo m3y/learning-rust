cdd33b26b2a62d974db155d9714e66e85d6cee26
m3y <ma3ya.ozw+github@gmail.com>
2020-09-22 00:30:07 +0900

    add runtime sample

---
+++ gear_book/runtime/src/main.rs
@@ -0,0 +1,10 @@
+async fn add(left: i32, right: i32) -> i32 {
+    left + right
+}
+
+//#[tokio::main]
+#[async_std::main]
+async fn main() {
+    let ans = add(2, 3).await;
+    println!("{}", ans);
+}
